1f831562336576928d3eca3672bfdcc90d77d0e5
molly <mollydalzell@gmail.com>
2023-06-21 21:17:14 -0400

    js stuff

--- app.js
+++ app.js
@@ -1,3 +1,67 @@
 function alertMe() {
     alert('Hi!');
 }
+
+
+// The DOM
+// document is your html
+// window is the browser window (current tab)
+// you can use these key words for methods like window.scrollTo()
+
+
+window.innerHeight // give visible height of html window
+window.outerHeight // gives height of entire browser window
+
+window.localStorage // temporary data that is stored in the window
+
+localStorage.setItem('test', 20);
+
+window.scrollX // gives how much we've scrolled to the left
+window.scrolly // gives how much we've scrolled up and down
+
+/* Event listener
+
+window.onscroll = function() {
+    console.log('hi');
+}
+
+*/
+
+// Using DOM API to add content to our html
+
+const elementNode = document.createElement('p');
+const textNode = document.createTextNode('Content');
+const attributeNode = document.createAttribute('class');
+attributeNode.value = 'some.class';
+
+elementNode.appendChild(textNode);
+elementNode.setAttributeNode(attributeNode);
+
+document.body.appendChild(elementNode);
+
+// three ways to return our second button
+document.querySelector('button');
+document.querySelector('#btn-1');
+document.querySelector('.my-btn');
+
+
+const btn = document.querySelector('#btn-1');
+
+function addParagraph() {
+    
+    // console.log('the button is working')
+    const randomNum = Math.floor(Math.random()*100);
+    const pContent = 'The random number is: ' + randomNum;
+
+    const newElement = document.createElement('p');
+    newElement.textContent = pContent;
+
+    document.body.appendChild(newElement);
+
+}
+
+// two new ways to add event listener to our button
+//btn.addEventListener('click', addParagraph);
+btn.onclick = addParagraph;
+
+const list = document.querySelector('ol');
