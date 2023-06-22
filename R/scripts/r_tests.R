3e5bb702935daed2fbc4899a21945102aeb3191c
mollydalzell <mollydalzell@gmail.com>
2023-06-22 11:49:30 -0400

    r stuff

--- R/scripts/r_tests.R
+++ R/scripts/r_tests.R
@@ -4,7 +4,13 @@ library(tidyverse)
 
 test <- read_csv('data/wta_matches_2000s.csv')
 
+getmode <- function(v) {
+    uniqv <- unique(v)
+    uniqv[which.max(tabulate(match(v, uniqv)))]
+}
 
 test |>
 group_by(tourney_name) |>
-summarize(most_common_hand = mode(winner_hand))
+summarize(most_common_hand = getmode(winner_hand)) |>
+filter(most_common_hand == 'L') |>
+print(n = 177)
